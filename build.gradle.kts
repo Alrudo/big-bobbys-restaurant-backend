022fb9e85fadc419e89582766a6f5bbc1a95110c
sesaal <sesaal@ttu.ee>
2020-10-11 15:05:02 +0300

    Added configuration of h2 to application.properties for #14 and added some JPA dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,16 +13,20 @@ java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
 	mavenCentral()
+	jcenter()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
 	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 	}
+
 	testImplementation("org.springframework.security:spring-security-test")
 }
 
