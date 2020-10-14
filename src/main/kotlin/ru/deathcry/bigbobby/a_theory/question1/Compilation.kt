9373514eef47060449866e476772340a12faef5e
sesaal <sesaal@ttu.ee>
2020-10-14 22:23:36 +0300

    Answers to question1

--- src/main/kotlin/ru/deathcry/bigbobby/a_theory/question1/Compilation.kt
+++ src/main/kotlin/ru/deathcry/bigbobby/a_theory/question1/Compilation.kt
@@ -5,12 +5,14 @@ class Compilation { //todo
     //todo p1
     // In your words (do not use wiki definitions)
     // What is compilation?
-    // Answer:
+    // Answer: Code that is readable by human is transformed into bytecode that specific computer understands
+
     //todo p2
     // Name 2 benefits of compilation?
-    // 1
-    // 2
+    // 1) Usually very efficient, especially if compiler is optimizing the output.
+    // 2) It does not require anything else other than OS to run, self-contained
+
     //todo p3
     // Name 1 disadvantage of compilation?
-    // 1
+    // 1) Larger programs need a lot of time to compile from source code.
 }
--- src/main/kotlin/ru/deathcry/bigbobby/a_theory/question6/sheep/Sheep.kt
+++ src/main/kotlin/ru/deathcry/bigbobby/a_theory/question6/sheep/Sheep.kt
@@ -1,3 +1,3 @@
 package ru.deathcry.bigbobby.a_theory.question6.sheep
 
-class Sheep constructor(val id: Long, var name: String, var age: Int, val color: String)
+data class Sheep(val id: Long, var name: String, var age: Int, val color: String)
