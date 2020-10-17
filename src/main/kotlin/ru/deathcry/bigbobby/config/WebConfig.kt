187faf08f3875621e271a4ba54c1230908e5ac5f
alrudo <alrudo@ttu.ee>
2020-10-17 17:27:50 +0300

    Added CORS configuration to allow any website to use our api

---
+++ src/main/kotlin/ru/deathcry/bigbobby/config/WebConfig.kt
@@ -0,0 +1,14 @@
+package ru.deathcry.bigbobby.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+@EnableWebMvc
+class WebConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**").allowedOrigins("*")
+    }
+}
