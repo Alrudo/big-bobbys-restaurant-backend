50281114a063ccc41ab292a96c8ed466fb3ff125
alrudo <alrudo@ttu.ee>
2021-01-09 21:20:40 +0200

    Added theory question14

---
+++ src/main/kotlin/ru/deathcry/bigbobby/c_theory/question14/blogs/Author.java
@@ -0,0 +1,7 @@
+package ru.deathcry.bigbobby.c_theory.question14.blogs;
+
+public class Author {
+    private String displayName;
+    private String realName;
+    private Integer age;
+}
---
+++ src/main/kotlin/ru/deathcry/bigbobby/c_theory/question14/lessons/Students.java
@@ -0,0 +1,5 @@
+package ru.deathcry.bigbobby.c_theory.question14.lessons;
+
+public class Students {
+    private String name;
+}
