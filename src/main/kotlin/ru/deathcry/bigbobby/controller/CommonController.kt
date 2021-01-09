a98eba290e6cbb3ea9d7a7902970c42265ddd17e
sesaal <sesaal@ttu.ee>
2021-01-09 22:50:14 +0200

    fix for cicd

--- src/main/kotlin/ru/deathcry/bigbobby/controller/CommonController.kt
+++ src/main/kotlin/ru/deathcry/bigbobby/controller/CommonController.kt
@@ -1,11 +1,9 @@
 package ru.deathcry.bigbobby.controller
 
-import org.springframework.security.core.Authentication
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import ru.deathcry.bigbobby.dto.MenuItemDto
-import ru.deathcry.bigbobby.model.CustomerEntity
 import ru.deathcry.bigbobby.service.KitchenService
 import ru.deathcry.bigbobby.util.morph
 
@@ -21,9 +19,8 @@ class CommonController(
     }
 
     @GetMapping("/ping")
-    fun ping(authentication: Authentication): String {
-        val userDetails = authentication.principal as CustomerEntity
-        return "OK + " + userDetails.getAuthorities()
+    fun ping(): String {
+        return "OK"
     }
        
 }
