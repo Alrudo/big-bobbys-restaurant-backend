c5702cdc39413d730030541a9548c5a85f35849d
alrudo <alrudo@ttu.ee>
2021-01-09 21:18:37 +0200

    Added theory questions.

---
+++ src/main/kotlin/ru/deathcry/bigbobby/c_theory/question12/Security.java
@@ -0,0 +1,21 @@
+package ru.deathcry.bigbobby.c_theory.question12;
+
+public class Security {
+
+    //todo A
+    // What is authentication?
+    // Answer: process of making sure something is valid/true. For example, proving user identity by asking his set password,
+    // checking the file checksum etc.
+
+    //todo B
+    // What is authorization?
+    // Answer: giving the rights to the user to do some kind of actions as well as checking if user has these rights when
+    // he attempts to do any kind of actions that require these rights.
+
+    //todo C
+    // For web (HTTP backend) Spring Security is configured as a ... ? Explain it.
+    // Think or read how Spring Security checks are injection into your application.
+    // Configured as (one word):
+    // Description:
+
+}
---
+++ src/main/kotlin/ru/deathcry/bigbobby/c_theory/question13/MaterialDesign.java
@@ -0,0 +1,13 @@
+package ru.deathcry.bigbobby.c_theory.question13;
+
+public class MaterialDesign {
+
+    //todo A
+    // What is material design?
+    // Answer: Material Design is a graphic design style for software and application interfaces developed by Google.
+
+    //todo B
+    // Name and describe 2 principles of material design (there are more than 2)
+    // 1
+    // 2
+}
